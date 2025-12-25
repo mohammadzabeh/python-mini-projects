45c234b3b7e32419f5b7146f1476e2ba7fffeb2b
mohammadzabeh <mohammad.s.zabeh@gmail.com>
2025-12-25 14:44:23 +0330

    Feat: Add command-line calculator project

---
+++ 01-command-line-calculator/README.md
@@ -0,0 +1,11 @@
+# 01: Command-Line Calculator
+
+A simple terminal-based calculator built to practice foundational Python skills.
+
+### Key Concepts Practiced:
+
+-   User Input (`input()`)
+-   Conditional Logic (`if/elif/else`)
+-   Data Type Conversion (`float()`)
+-   F-String Formatting
+-   Loop (while)
---
+++ 01-command-line-calculator/main.py
@@ -0,0 +1,28 @@
+# a calculator for simple operations.
+
+while True:
+    x = float(input("first number: ").strip())    
+    op = (input("operation: ").strip())
+    if op == "*":
+        y = float(input("second number: ").strip())
+        e = x * y
+        print(f"the result of {x} * {y} is: {e}")
+        break
+    elif op == "/":
+        y = float(input("second number: ").strip())
+        e = x / y
+        print(f"the result of {x} / {y} is: {e}")
+        break
+    elif op == "+":
+        y = float(input("second number: ").strip())
+        e = x + y
+        print(f"the result of {x} + {y} is: {e}")
+        break
+    elif op == "-":
+        y = float(input("second number: ").strip())
+        e = x - y
+        print(f"the result of {x} - {y} is: {e}")
+        break
+    else:
+        print("invalid operation!!! [+, -, *, /]")
+    
